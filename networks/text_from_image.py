4826147dbf738ce8952ae80c7dcab63efff85f72
AshRP03 <ashwinrp03@gmail.com>
2023-10-28 17:46:02 -0500

    Image To Text Function
    Takes in a PIL Image and outputs a string of text, which correspond to any words or captions in the image itself.

--- networks/text_from_image.py
+++ networks/text_from_image.py
@@ -1,16 +1,20 @@
 import os
 from google.cloud import vision
 from google.cloud.vision_v1 import types
+import io
+from PIL import Image
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/ash/Documents/GitHub_Projects/tamu-datathon-23-sub-project-fde75f68cf19.json"
 
 client = vision.ImageAnnotatorClient()
 
-with open("/Users/ash/Desktop/umm.png", 'rb') as image_file:
-    content = image_file.read()
+def text_from_PIL_image(filename):
 
-image = types.Image(content=content)
-response = client.text_detection(image=image)
+    img_byte_array = io.BytesIO()
+    image_data = img_byte_array.getvalue()
 
-texts = response.text_annotations
-print(texts[0].description)
+    image = types.Image(content=image_data)
+    response = client.text_detection(image=image)
+
+    texts = response.text_annotations
+    return(texts[0].description)
