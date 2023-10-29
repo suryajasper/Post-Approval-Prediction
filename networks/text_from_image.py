5d464ca58070d9a31ba8a45bff9fd74083e2bce0
AshRP03 <ashwinrp03@gmail.com>
2023-10-28 20:25:03 -0500

    Fixed gitignore as well as include and imports on torch and google vision

--- networks/text_from_image.py
+++ networks/text_from_image.py
@@ -1,20 +1,27 @@
 import os
+import io
+import torch
+import torchvision.transforms as transforms
+from PIL import Image
 from google.cloud import vision
 from google.cloud.vision_v1 import types
-import io
-from PIL import Image
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/ash/Documents/GitHub_Projects/tamu-datathon-23-sub-project-fde75f68cf19.json"
 
 client = vision.ImageAnnotatorClient()
 
-def text_from_PIL_image(filename):
+def text_from_torch_tensor():
+
+    tensor_to_PIL = transforms.ToPILImage()
+    pil_image = tensor_to_PIL
 
     img_byte_array = io.BytesIO()
+    pil_image.save(img_byte_array, format='PNG')
     image_data = img_byte_array.getvalue()
 
     image = types.Image(content=image_data)
     response = client.text_detection(image=image)
 
     texts = response.text_annotations
-    return(texts[0].description)
+    if texts:
+        return texts[0].description
